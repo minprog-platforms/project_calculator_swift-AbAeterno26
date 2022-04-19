5763a91e98594e0bf2ad351788124ae73dc9e2d6
Sarah Blok <nathanchress@hotmail.com>
2022-04-19 11:16:29 +0200

    Created XCode Project

---
+++ Shared/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Shared
+//
+//  Created by Sarah Blok on 19/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
