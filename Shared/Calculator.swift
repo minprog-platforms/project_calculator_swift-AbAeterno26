32ba31063a0d0963af3180bed6152f44b4eb5d4a
Sarah Blok <nathanchress@hotmail.com>
2022-04-22 19:10:19 +0200

    UI is finished, logic needs to go in separate swift file inside structure and needs additional functions.

---
+++ Shared/Calculator.swift
@@ -0,0 +1,72 @@
+//
+//  Calculator.swift
+//  Calculator
+//
+//  Created by Sarah Blok on 22/04/2022.
+//
+
+import Foundation
+import SwiftUI
+
+struct Calculator {
+    // Enumeration for special operations
+    enum Operation {
+        case add, subtract, multiply, divide, none
+    }
+    
+    @State var value = "0"
+    @State var runningNumber = 0
+    @State var currentOperation: Operation = .none
+    
+    // Functions for buttons
+    mutating func didTap(button: CalcButton) {
+        switch button {
+        case .add, .subtract, .multiply, .divide, .equal:
+            if button == .add {
+                self.currentOperation = .add
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .subtract {
+                self.currentOperation = .subtract
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .multiply {
+                self.currentOperation = .multiply
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .divide {
+                self.currentOperation = .divide
+                self.runningNumber = Int(self.value) ?? 0
+            }
+            else if button == .equal {
+                let runningValue = self.runningNumber
+                let currentValue = Int(self.value) ?? 0
+                switch self.currentOperation {
+                case .add: self.value = "\(runningValue + currentValue)"
+                case .subtract: self.value = "\(runningValue - currentValue)"
+                case .multiply: self.value = "\(runningValue * currentValue)"
+                case .divide: self.value = "\(runningValue / currentValue)"
+                case .none:
+                    break
+                }
+            }
+            
+            if button != .equal {
+                self.value = "0"
+            }
+            
+        case .clear:
+            self.value = "0"
+        case .decimal, .negative, .percent:
+            break
+        default:
+            let number = button.rawValue
+            if self.value == "0" {
+                value = number
+            }
+            else {
+                self.value = "\(self.value)\(number)"
+            }
+        }
+    }
+}
